dd76e0f69b2bf47b38de04a1b1c3ba7206f46a66
Eduardo Foncesca Finardi <129402056+EduardoFoncescaFinardi@users.noreply.github.com>
2023-08-09 10:59:08 -0300

    Add files via upload

---
+++ aula2.2/dictionary.py
@@ -0,0 +1,16 @@
+# {chave:valor} -
+# lista = ()
+# dicionario = {}
+
+# dicionario = {'primeiro':1, 'segundo':2, 'terceiro':3}
+
+# dic_1 = dict(primeiro = 1, segundo = 2, terceiro  = 3)
+
+# zip = contatenar listas
+
+# zip(['priemiro', 'segundo', 'terceiro'])
+
+# list(zip(['primeiro', 'segundo', 'terceiro'], [1,2,3]))
+
+# dict(zip(['primeiro', 'segundo', 'terceiro'], [1,2,3]))
+
