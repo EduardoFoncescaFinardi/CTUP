8d4d81a9d8a630b9bb641309052ee4afa99c11c8
Eduardo Foncesca Finardi <129402056+EduardoFoncescaFinardi@users.noreply.github.com>
2023-08-16 11:45:48 -0300

    Add files via upload

---
+++ COMPUTATIONAL THINKING USING PYTHON/códigos_aula/aula _4-2.py
@@ -0,0 +1,16 @@
+'''
+def teste(n, s):
+    n =  n +2
+    s[0] = s[0] +  1
+
+n1 = 1
+lista = [1]
+teste(n1, lista)
+print('fim!')
+'''
+
+notas_python = [5,8,2,10]
+notas_Java = [3,5,2,5]
+
+dic = {'Python':notas_python, 'Java': notas_Java}
+
